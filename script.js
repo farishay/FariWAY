039502a34fc49bde33c2b40981851d8d72077ecc
Farwa Tariq <farwa.tariq2434@gmail.com>
2025-05-18 20:42:13 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("🌍 Welcome to Fariway!");
+let response = confirm("\nReady to discover your next dream destination?");
+if (response) {
+    alert("✈️ Awesome! Let's start your journey...");
+} else {
+    alert("😴 No worries! The world will wait for you 🌙");
+}
